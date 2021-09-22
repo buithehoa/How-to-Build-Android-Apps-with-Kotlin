28ea23dedb9d8e1845128c05439a1b35d81a91e6
Bui The Hoa <buithehoa@gmail.com>
2021-09-22 16:31:41 +0700

    Load web view.

--- Chapter01/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ Chapter01/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(getWebView())
+    }
+
+    private fun getWebView(): WebView {
+        val webView = WebView(this)
+        webView.settings.javaScriptEnabled = true
+        webView.loadUrl("https://www.twitter.com")
+        return webView
     }
 }
