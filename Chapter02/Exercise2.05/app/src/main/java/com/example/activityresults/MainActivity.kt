f31630f6b02ba210645462aacd3539e3ceb677cb
Bui The Hoa <buithehoa@gmail.com>
2021-10-06 21:51:56 +0700

    Start activity for result.

--- Chapter02/Exercise2.05/app/src/main/java/com/example/activityresults/MainActivity.kt
+++ Chapter02/Exercise2.05/app/src/main/java/com/example/activityresults/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.activityresults
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+
+const val PICK_RAINBOW_COLOR_INTENT = 1
+const val RAINBOW_COLOR_NAME = "RAINBOW_COLOR_NAME"
+const val RAINBOW_COLOR = "RAINBOW_COLOR"
+const val DEFAULT_COLOR = "#FFFFFF"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.submit_button).setOnClickListener {
+            Intent(this, RainbowColorPickerActivity::class.java)
+                .also { rainbowColorPickerIntent ->
+                    startActivityForResult(
+                        rainbowColorPickerIntent,
+                        PICK_RAINBOW_COLOR_INTENT
+                    )
+                }
+        }
     }
+
+
 }
---
+++ Chapter02/Exercise2.05/app/src/main/java/com/example/activityresults/RainbowColorPickerActivity.kt
@@ -0,0 +1,11 @@
+package com.example.activityresults
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RainbowColorPickerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_rainbow_color_picker)
+    }
+}
