cf44f124ece367850c2f2ec23ce7b3579c8a53cd
Bui The Hoa <buithehoa@gmail.com>
2021-10-07 17:41:09 +0700

    Lay out color picker.

--- Chapter02/Exercise2.05/app/src/main/java/com/example/activityresults/MainActivity.kt
+++ Chapter02/Exercise2.05/app/src/main/java/com/example/activityresults/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.example.activityresults
 
+import android.app.Activity
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.core.view.isVisible
 
 const val PICK_RAINBOW_COLOR_INTENT = 1
 const val RAINBOW_COLOR_NAME = "RAINBOW_COLOR_NAME"
@@ -26,5 +31,21 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
 
+        if (requestCode == PICK_RAINBOW_COLOR_INTENT && resultCode == Activity.RESULT_OK) {
+            val backgroundColor = data?.getIntExtra(
+                RAINBOW_COLOR,
+                Color.parseColor(DEFAULT_COLOR)
+            ) ?: Color.parseColor(DEFAULT_COLOR)
+            val colorName = data?.getStringExtra(RAINBOW_COLOR_NAME) ?: ""
+            val colorMessage = getString(R.string.color_chosen_message, colorName)
+
+            val rainbowColor = findViewById<TextView>(R.id.rainbow_color)
+            rainbowColor.setBackgroundColor(ContextCompat.getColor(this, backgroundColor))
+            rainbowColor.text = colorMessage
+            rainbowColor.isVisible = true
+        }
+    }
 }
