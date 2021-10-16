599c44180348d6585b6759e29094e44aeebd565e
Bui The Hoa <buithehoa@gmail.com>
2021-10-16 18:29:15 +0700

    Create a list of star signs.

--- Chapter03/Exercise3.03/app/src/main/java/com/example/dualpanelayouts/ListFragment.kt
+++ Chapter03/Exercise3.03/app/src/main/java/com/example/dualpanelayouts/ListFragment.kt
@@ -1,10 +1,12 @@
 package com.example.dualpanelayouts
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import java.lang.RuntimeException
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -16,10 +18,44 @@ private const val ARG_PARAM2 = "param2"
  * Use the [ListFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class ListFragment : Fragment() {
+class ListFragment : Fragment(), View.OnClickListener {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    private lateinit var starSignListener: StarSignListener
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val starSigns = listOf<View>(
+            view.findViewById(R.id.aquarius),
+            view.findViewById(R.id.pisces),
+            view.findViewById(R.id.aries),
+            view.findViewById(R.id.taurus),
+            view.findViewById(R.id.gemini),
+            view.findViewById(R.id.cancer),
+            view.findViewById(R.id.leo),
+            view.findViewById(R.id.virgo),
+            view.findViewById(R.id.libra),
+            view.findViewById(R.id.scorpio),
+            view.findViewById(R.id.sagittarius),
+            view.findViewById(R.id.capricorn),
+        )
+
+        starSigns.forEach {
+            it.setOnClickListener(this)
+        }
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+
+        if (context is StarSignListener) {
+            starSignListener = context
+        } else {
+            throw RuntimeException("Must implement StarSignListener")
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -55,5 +91,11 @@ class ListFragment : Fragment() {
                     putString(ARG_PARAM2, param2)
                 }
             }
+    }
+
+    override fun onClick(v: View?) {
+        v?.let { starSign ->
+            starSignListener.onSelected(starSign.id)
+        }
     }
 }
