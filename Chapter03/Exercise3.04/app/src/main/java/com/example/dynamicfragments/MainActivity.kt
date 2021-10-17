5190e3a06503e198245de3008d2d0e812b386d3f
Bui The Hoa <buithehoa@gmail.com>
2021-10-17 16:16:54 +0700

    Add fragment list dynamically.

--- Chapter03/Exercise3.04/app/src/main/java/com/example/dynamicfragments/MainActivity.kt
+++ Chapter03/Exercise3.04/app/src/main/java/com/example/dynamicfragments/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.dynamicfragments
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentContainerView
 
-class MainActivity : AppCompatActivity() {
+const val STAR_SIGN_ID = "STAR_SIGN_ID"
+
+interface StarSignListener {
+    fun onSelected(id: Int)
+}
+
+class MainActivity : AppCompatActivity(), StarSignListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            findViewById<FragmentContainerView>(R.id.fragment_container)?.let {
+                val listFragment = ListFragment()
+                supportFragmentManager.beginTransaction().add(it.id, listFragment).commit()
+            }
+        }
+    }
+
+    override fun onSelected(id: Int) {
+        TODO("Not yet implemented")
     }
 }
