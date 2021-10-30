404c3afa8e8fda0ff41b47abe492eaddec70b6c4
Bui The Hoa <buithehoa@gmail.com>
2021-10-30 14:22:06 +0700

    Parse and collect image URL.

--- Chapter05/Exercise5.01/app/src/main/java/com/example/catagentprofile/MainActivity.kt
+++ Chapter05/Exercise5.01/app/src/main/java/com/example/catagentprofile/MainActivity.kt
@@ -1,21 +1,24 @@
 package com.example.catagentprofile
 
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.TextView
 import com.example.catagentprofile.api.TheCatApiService
+import com.example.catagentprofile.model.ImageResultData
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.converter.scalars.ScalarsConverterFactory
 
 class MainActivity : AppCompatActivity() {
     private val retrofit by lazy {
         Retrofit.Builder()
             .baseUrl("https://api.thecatapi.com/v1/")
-            .addConverterFactory(ScalarsConverterFactory.create())
+            .addConverterFactory(MoshiConverterFactory.create())
             .build()
     }
 
@@ -36,14 +39,16 @@ class MainActivity : AppCompatActivity() {
 
     private fun getCatImageResponse() {
         val call = theCatApiService.searchImages(1, "full")
-        call.enqueue(object : Callback<String> {
-            override fun onFailure(call: Call<String>, t: Throwable) {
+        call.enqueue(object : Callback<List<ImageResultData>> {
+            override fun onFailure(call: Call<ImageResultData>, t: Throwable) {
                 Log.e("MainActivity", "Failed to get search results", t)
             }
 
-            override fun onResponse(call: Call<String>, response: Response<String>) {
+            override fun onResponse(call: Call<ImageResultData>, response: Response<List<ImageResultData>>) {
                 if (response.isSuccessful) {
-                    serverResponseView.text = response.body()
+                    val imageResults = response.body()
+                    val firstImageUrl = imageResults?.firstOrNull()?.imageUrl ?: "No URL"
+                    serverResponseView.text = "Image URL: $firstImageUrl"
                 } else {
                     Log.e("MainActivity", "Failed to get search results\n${response.errorBody()?.string() ?: ""}")
                 }
