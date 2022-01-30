d293201d2e6327bff1ec82dd460f93627de19655
Bui The Hoa <buithehoa@gmail.com>
2022-01-30 12:37:09 +0700

    Update API URL.

--- Chapter05/Activity501/app/src/main/java/com/example/saigonweather/api/OpenWeatherMapService.kt
+++ Chapter05/Activity501/app/src/main/java/com/example/saigonweather/api/OpenWeatherMapService.kt
@@ -1,12 +1,13 @@
 package com.example.saigonweather.api
 
+import com.example.saigonweather.model.openweathermap.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface OpenWeatherMapService {
-    @GET("data/2.5/weather")
+    @GET("weather")
     fun getWeather(
         @Query("q") q: String,
         @Query("appid") appid: String
-    )
+    ) : Call<Response>
 }
