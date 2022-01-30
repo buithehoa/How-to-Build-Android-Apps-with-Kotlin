e4b7d7967c2fc3ab3b2e8e4689ffc868d84b6ecb
Bui The Hoa <buithehoa@gmail.com>
2022-01-30 13:40:35 +0700

    Add the view.

--- Chapter05/Activity501/app/src/main/java/com/example/saigonweather/MainActivity.kt
+++ Chapter05/Activity501/app/src/main/java/com/example/saigonweather/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.saigonweather
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 import com.example.saigonweather.api.OpenWeatherMapService
 import com.example.saigonweather.model.openweathermap.ResponseData
 import retrofit2.Call
@@ -10,6 +12,19 @@ import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 
 class MainActivity : AppCompatActivity() {
+    private val titleView: TextView by lazy {
+        findViewById(R.id.main_title)
+    }
+    private val statusView: TextView by lazy {
+        findViewById(R.id.main_status)
+    }
+    private val descriptionView: TextView by lazy {
+        findViewById(R.id.main_description)
+    }
+    private val weatherIconView: ImageView by lazy {
+        findViewById(R.id.main_weather_icon)
+    }
+
     private val retrofit by lazy {
         Retrofit.Builder()
             .baseUrl("https://api.openweathermap.org/data/2.5/")
