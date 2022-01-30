6d69c26490e93f504eded3f8191d1f7c5fcfffb6
Bui The Hoa <buithehoa@gmail.com>
2022-01-30 12:52:32 +0700

    Update Gradle config.

--- Chapter05/Activity501/app/src/main/java/com/example/saigonweather/MainActivity.kt
+++ Chapter05/Activity501/app/src/main/java/com/example/saigonweather/MainActivity.kt
@@ -2,8 +2,22 @@ package com.example.saigonweather
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.saigonweather.api.OpenWeatherMapService
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 
 class MainActivity : AppCompatActivity() {
+    private val retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl("https://api.openweathermap.org/data/2.5/")
+            .addConverterFactory(MoshiConverterFactory.create())
+            .build()
+    }
+
+    private val weatherApiService by lazy {
+        retrofit.create(OpenWeatherMapService::class.java)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
--- Chapter05/Activity501/app/src/main/java/com/example/saigonweather/api/OpenWeatherMapService.kt
+++ Chapter05/Activity501/app/src/main/java/com/example/saigonweather/api/OpenWeatherMapService.kt
@@ -1,6 +1,7 @@
 package com.example.saigonweather.api
 
 import com.example.saigonweather.model.openweathermap.Response
+import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
 
